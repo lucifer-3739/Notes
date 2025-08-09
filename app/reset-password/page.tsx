48684e00f8a508c1331773c6a36f2ea6aa660cd8
Sourabh <sourabhlucifer9087@gmail.com>
2025-08-09 17:18:03 +0530

    complete auth with google login

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/forms/login-form"
+import { ResetPasswordForm } from "@/components/forms/reset-password-form"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <ResetPasswordForm />
+      </div>
+    </div>
+  )
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,34 +1,47 @@
+
+import PasswordResetEmail from "@/components/email/reset-email";
+import VerificationEmail from "@/components/email/verification-email";
 import { db } from "@/db/drizzle";
+import { schema } from "@/db/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { schema } from "@/db/schema";
 import { nextCookies } from "better-auth/next-js";
 import { Resend } from "resend";
-import VerificationEmail from "@/components/email/verification-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
-  emailVerification: {
-		sendVerificationEmail: async ({user, url, token}, request) => {
-      const { data, error } = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: user.email,
-        subject: 'Verify your email',
-        react: VerificationEmail({
-          userName: user.name, 
-          verificationUrl: url,
-        })
-      })
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url }) => {
+            await resend.emails.send({
+                from: 'NoteForge <noteforge@orcdev.com>',
+                to: [user.email],
+                subject: 'Verify your email address',
+                react: VerificationEmail({ userName: user.name, verificationUrl: url }),
+            });
+        },
+        sendOnSignUp: true,
     },
-    sendOnSignUp: true,
-	},
-  database: drizzleAdapter(db, {
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
+    emailAndPassword: {
+        enabled: true,
+        sendResetPassword: async ({ user, url }) => {
+            await resend.emails.send({
+                from: 'NoteForge <noteforge@orcdev.com>',
+                to: [user.email],
+                subject: 'Reset your password',
+                react: PasswordResetEmail({ userName: user.name, resetUrl: url, requestTime: new Date().toLocaleString() }),
+            });
+        },
+    },
+    database: drizzleAdapter(db, {
         provider: "pg",
         schema
     }),
-  plugins: [nextCookies()],
+    plugins: [nextCookies()]
 });
